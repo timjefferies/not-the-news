2bf338d3221aec6f15a108fbc30a57406da511f9
Ubuntu <ubuntu@instance-20211211-1534>
2023-06-18 07:44:20 +0000

    center login on mobile

--- run.sh
+++ run.sh
@@ -1,12 +1,10 @@
 python3 merge_feeds.py
 python3 filter_feed.py
 sed -i 's/twitter.com/nitter.net/g' /tmp/filtered_feed.xml;
-sed -i 's/www.reddit/i.reddit/g' /tmp/filtered_feed.xml;
 sed -i 's/img src/img loading="lazy" src/g' /tmp/filtered_feed.xml; # lazy load images
 sed -i 's/<br>/<br><br>/g' /tmp/filtered_feed.xml;
-sed -i 's/www.reddit/i.reddit/g' /tmp/filtered_feed.xml;
-sed -i 's/ target=\"_blank\" //g' /tmp/filtered_feed.xml;
-
+sed -i 's/www.reddit/old.reddit/g' /tmp/filtered_feed.xml;
+mkdir -p www/data
 python3 make_html.py
 #minify www/data/final-rss.html
-chown -R www-data:www-data www
+sudo chown -R www-data:www-data www
